f084af453dfc2e536e728f1e747c2913f00de93b
shiwg <1052011493@qq.com>
2019-04-08 11:17:29 +0800

    第一次提交

---
+++ src/gitdemo/Hello_world.java
@@ -0,0 +1,20 @@
+/**   
+ * @Title:Hello_world.java
+ * @Description: </p>
+ * 
+ * @Company: 
+ * @author shiwg
+ * @date 2019年4月8日
+ * @version 1.0   */  
+package gitdemo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Hello_world {
+	public static void main(String[] args) {
+		
+	}
+
+}
